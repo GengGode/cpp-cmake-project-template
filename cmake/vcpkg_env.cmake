dccbebf6ef5316a2f05eecf278bb1648856e0ddd
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2025-06-08 20:21:19 +0800

    优化 vcpkg 环境配置，简化目标三元组设置逻辑

--- cmake/vcpkg_env.cmake
+++ cmake/vcpkg_env.cmake
@@ -10,39 +10,39 @@ macro(set_vcpkg_config)
             set(VCPKG_TARGET_TRIPLET "x64-linux" CACHE STRING "Vcpkg target triplet")
         endif()
     endif()
-    if(NOT DEFINED VCPKG_TARGET_TRIPLET)
-        if(WIN32)
-            set(VCPKG_TARGET_TRIPLET "x64-windows" CACHE STRING "Vcpkg target triplet")
-        else()
-            set(VCPKG_TARGET_TRIPLET "x64-linux" CACHE STRING "Vcpkg target triplet")
-        endif()
-    endif()
-    set(VCPKG_TARGET_TRIPLET "x64-windows" CACHE STRING "Vcpkg target triplet")
+    set(VCPKG_TARGET_TRIPLET $ENV{VCPKG_TARGET_TRIPLET} CACHE STRING "Vcpkg target triplet")
     set(VCPKG_MANIFEST_INSTALL ON CACHE BOOL "Enable manifest install")
     set(VCPKG_APPLOCAL_DEPS ON CACHE BOOL "Enable applocal deps")
     set(VCPKG_INSTALLED_DIR ${CMAKE_BINARY_DIR}/vcpkg_installed)
 endmacro(set_vcpkg_config)
 
-
 # load VCPKG_ROOT from the environment
 if(DEFINED ENV{VCPKG_ROOT})
-    set_vcpkg_config()
-    message(STATUS "VCPKG_ROOT found in the environment")
+    message(STATUS "VCPKG_ROOT found in the environment, using: $ENV{VCPKG_ROOT}")
 else()
     # load VCPKG_ROOT from the .vcpkg-root file
     if(EXISTS "${CMAKE_SOURCE_DIR}/.vcpkg-root")
         file(STRINGS "${CMAKE_SOURCE_DIR}/.vcpkg-root" VCPKG_ROOT)
-        if(NOT EXISTS "${VCPKG_ROOT}")
-            message(FATAL_ERROR "VCPKG_ROOT not found in the .vcpkg-root file")
-        endif()
         set(ENV{VCPKG_ROOT} "${VCPKG_ROOT}")
-        set_vcpkg_config()
-
-        message(STATUS "VCPKG_ROOT found in the .vcpkg-root file")
+        message(STATUS "VCPKG_ROOT found in the .vcpkg-root file, using: $ENV{VCPKG_ROOT}")
     else()
         message(WARNING "VCPKG_ROOT not found in the environment or .vcpkg-root file
             Please set VCPKG_ROOT in the environment or create a .vcpkg-root file
             with the path to the vcpkg installation directory.
-            For example, C:/Users/Public/vcpkg")
+            For example, C:/vcpkg")
     endif()
 endif()
+
+# load VCPKG_TARGET_TRIPLET from the environment
+if(DEFINED ENV{VCPKG_TARGET_TRIPLET})
+    message(STATUS "VCPKG_TARGET_TRIPLET found in the environment: ${VCPKG_TARGET_TRIPLET}")
+else()
+    # load VCPKG_TARGET_TRIPLET from the .vcpkg-target-triplet file
+    if(EXISTS "${CMAKE_SOURCE_DIR}/.vcpkg-target-triplet")
+        file(STRINGS "${CMAKE_SOURCE_DIR}/.vcpkg-target-triplet" VCPKG_TARGET_TRIPLET)
+        set(ENV{VCPKG_TARGET_TRIPLET} "${VCPKG_TARGET_TRIPLET}")
+        message(STATUS "VCPKG_TARGET_TRIPLET found in the .vcpkg-target-triplet file: ${VCPKG_TARGET_TRIPLET}")
+    endif()
+endif()
+    
+set_vcpkg_config()
